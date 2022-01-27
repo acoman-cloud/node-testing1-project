01493f56d5c4f30d2aad5727d52426c12a900372
Anthony Coman <acoman@yahoo.com>
2022-01-26 19:38:54 -0600

    wip

--- index.js
+++ index.js
@@ -168,8 +168,7 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    const emptySpace = this.tank - this.gas
-    if (gallons <= emptySpace) {
+    if (gallons <= (this.tank - this.gas)) {
       this.gas += gallons
     } else {
       this.gas = this.tank
